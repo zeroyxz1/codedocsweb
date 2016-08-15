b9c3ae48bc43a5d2aff86366d9f2688a100cc7a9
zeroyxz <peterworlin@hotmail.com>
2016-08-15 16:38:38 +0100

    Updated

---
+++ CodeDocsSln/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeDocsSln.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About CodeDocs Cloud Services";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact CodeDocs regarding how your business can take advantage of the Cloud.";
+
+            return View();
+        }
+
+        public ActionResult FirstSteps()
+        {
+            ViewBag.Message = "Contact CodeDocs regarding how your business can take advantage of the Cloud.";
+
+            return View();
+        }
+
+        public ActionResult NextSteps()
+        {
+            ViewBag.Message = "Contact CodeDocs regarding how your business can take advantage of the Cloud.";
+
+            return View();
+        }
+
+        public ActionResult Optimise()
+        {
+            ViewBag.Message = "Contact CodeDocs regarding how your business can take advantage of the Cloud.";
+
+            return View();
+        }
+    }
+}
